97e5f3e0b6d7afde6611bf6a0d28d78c4075ea6c
Alan Loh <alan.loh.up7@gmail.com>
2019-10-22 13:47:46 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import re
+import nenupytv
+
+meta_file = open('nenupytf/metadata.py').read()
+metadata  = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", meta_file))
+
+setup(
+    name = 'nenupytv',
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = ['numpy', 'astropy'],
+    python_requires = '>=3.5',
+    scripts = [],
+    version = nenupytf.__version__,
+    description = 'NenuFAR-TV Python package',
+    url = 'https://github.com/AlanLoh/nenupy-tv.git',
+    author = metadata['author'],
+    author_email = metadata['email'],
+    license = 'MIT',
+    zip_safe = False
+    )
+
+# make the package:
+# python3 setup.py sdist bdist_wheel
+# upload it:
+# python3 -m twine upload dist/*version*
+
+# Release:
+# git tag -a v*version* -m "annotation for this release"
+# git push origin --tags
+
+
+# Install on nancep
+# /usr/local/bin/pip3.5 install nenupytv --install-option=--prefix=/cep/lofar/nenupytv --upgrade
