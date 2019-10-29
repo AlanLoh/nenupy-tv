1a91689e69acef97c730417d66d05020bd5618dc
Alan Loh <alan.loh.up7@gmail.com>
2019-10-29 13:27:00 +0100

    Few healpy functionnalities

--- nenupytv/image/grid.py
+++ nenupytv/image/grid.py
@@ -19,6 +19,80 @@ __all__ = [
     ]
 
 
-class Grid()
-    def __init__(self):
-        ...
+import healpy as hp
+import numpy as np
+
+
+# ============================================================= #
+# --------------------------- Grid ---------------------------- #
+# ============================================================= #
+class Grid():
+    """
+    """
+
+    def __init__(self, resolution):
+        self.nside = None
+        self.resol = resolution
+        self.ra, self.dec = self._hpx_radec()
+
+
+    # --------------------------------------------------------- #
+    # --------------------- Getter/Setter --------------------- #
+    @property
+    def resol(self):
+        """ Resolution of the cell in degrees
+        """
+        return self._resol
+    @resol.setter
+    def resol(self, r):
+        nsides = 2**np.arange(1, 12)
+        resol_rad = hp.nside2resol(
+            nside=nsides,
+            arcmin=False
+            )
+        resol_deg = np.degrees(resol_rad)
+        idx = (np.abs(resol_deg - r)).argmin()
+        if hasattr(self, 'resol'):
+            if resol_deg[idx] == self.resol:
+                # No need to recompute it
+                return
+        self._resol = resol_deg[idx]
+        self.nside = nsides[idx]
+        
+        return
+
+
+    @property
+    def nside(self):
+        return self._nside
+    @nside.setter
+    def nside(self, n):
+        if n is None:
+            self._nside = None
+        else:
+            self.npix = hp.nside2npix(n)
+            self._nside = n
+        return
+
+
+    # --------------------------------------------------------- #
+    # ------------------------ Methods ------------------------ #
+
+
+    # --------------------------------------------------------- #
+    # ----------------------- Internal ------------------------ #
+    def _hpx_radec(self):
+        """
+        """
+        ra, dec = hp.pix2ang(
+            nside=self.nside,
+            ipix=np.arange(self.npix),
+            lonlat=True,
+            nest=False
+            )
+        return ra, dec
+
+# ============================================================= #
+
+
+
