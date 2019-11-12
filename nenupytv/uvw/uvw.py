UVW computation accelerated and freq added (fixes #3)

nenupytv/uvw/uvw.py
@@ -151,35 +151,27 @@ class UVW(object):
             dec=dec
             )
 
-        # Compute the UVW coordinates for each baseline
-        # print('TESTING', self.bsl.shape)
-        # t0 = Time.now()
-        for k, (i, j) in enumerate(self.bsl):
-            dpos = self.positions[i] - self.positions[j]
-            xyz = rot_cel * np.matrix(dpos).T
-            uvw_k = rot_uvw * xyz
-            uvw[0, :, k, :] = rotz(uvw_k.T, 90) / self.wavel[:, np.newaxis]
-        # print('first method',(Time.now()-t0).sec)
-        # import matplotlib.pyplot as plt
-        # plt.plot(uvw[0,0,:,0], uvw[0,0,:,1], 'b.', linestyle='')
-        # plt.show()
-        # print('OLD UVW', uvw.shape)
+        # Initialize the UVW array
+        uvw = np.zeros(
+            (
+                1,
+                self.instru.ma.size,
+                self.instru.ma.size,
+                3
+            )
+        )
+
+        # Perform UVW computation for each baseline and frequency
+        xi = self.instru.tri_x
+        yi = self.instru.tri_y
+        dpos = self.positions[xi] - self.positions[yi]
+        xyz = rot_cel * np.matrix(dpos).T
+        uvw_k = rot_uvw * xyz
+        uvw[0, xi, yi, :] = uvw_k.T
+        uvw[0, yi, xi, :] = -uvw_k.T
+        uvw = uvw[:, np.newaxis, ...] /\
+            self.wavel[:, np.newaxis, np.newaxis, np.newaxis]
         
-        # THIS IS A WAAAAY FASTEST METHOD
-        # t0 = Time.now()
-        # xi, yi = np.tril_indices(self.instru.ma.size, 0)
-        # dpos = self.positions[xi] - self.positions[yi]
-        # xyz = rot_cel * np.matrix(dpos).T
-        # uvw_k = rot_uvw * xyz
-        # uvw = np.array(uvw_k)[:, :, np.newaxis] / self.wavel[np.newaxis , np.newaxis, :]
-        # print('second method',(Time.now()-t0).sec)
-        # print('NEW UVW', uvw.shape)
-
-        # plt.plot(uvw[0,:,0], uvw[1,:,0], 'b.', linestyle='')
-        # plt.plot(-uvw[0,:,0], -uvw[1,:,0], 'b.', linestyle='')
-        # plt.show()
-        # stop
-
         # Stack the UVW coordinates by time
         if self.uvw is None:
             self.uvw = uvw